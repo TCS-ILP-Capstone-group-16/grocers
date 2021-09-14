3754762d136fbc6781eea0441623f6f7de34887a
DXZ <774740493@qq.com>
2021-09-14 12:17:34 -0400

    default setup

---
+++ grocers/src/app/employee-sign-in/employee-sign-in.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-employee-sign-in',
+  templateUrl: './employee-sign-in.component.html',
+  styleUrls: ['./employee-sign-in.component.css']
+})
+export class EmployeeSignInComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
