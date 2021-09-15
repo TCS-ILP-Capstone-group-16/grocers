bcc22a07d798e78cdcfda66737a55e0ef382aed6
pnagbe <philipnagbe1@gmail.com>
2021-09-15 17:12:23 -0400

    added new compontents

--- grocers/src/app/app.module.ts
+++ grocers/src/app/app.module.ts
@@ -6,12 +6,18 @@ import { UserSignInComponent } from './user-sign-in/user-sign-in.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { UserSignUpComponent } from './user-sign-up/user-sign-up.component';
+import { AddEmployeeComponent } from './add-employee/add-employee.component';
+import { DeleteEmployeeComponent } from './delete-employee/delete-employee.component';
+import { GenerateReportsComponent } from './generate-reports/generate-reports.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserSignInComponent,
     UserSignUpComponent,
+    AddEmployeeComponent,
+    DeleteEmployeeComponent,
+    GenerateReportsComponent,
   ],
   imports: [
     BrowserModule,
