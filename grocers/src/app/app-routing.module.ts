52e836256e525c9aff1c1b537316f1f4f9514871
pnagbe <philipnagbe1@gmail.com>
2021-09-15 17:47:00 -0400

    resolved some conflicts

--- grocers/src/app/app-routing.module.ts
+++ grocers/src/app/app-routing.module.ts
@@ -1,14 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-<<<<<<< HEAD
 import { UserSignInComponent } from './user-sign-in/user-sign-in.component';
 import { UserSignUpComponent } from './user-sign-up/user-sign-up.component';
-
-const routes: Routes= [
-  { path: '', component: UserSignInComponent},
-  { path: 'signup', component: UserSignUpComponent}
-];
-=======
 import { AdminloginComponent } from './adminlogin/adminlogin.component';
 import { AdminhomeComponent } from './adminhome/adminhome.component';
 import { AddproductComponent } from './addproduct/addproduct.component';
@@ -17,12 +10,12 @@ import { UpdateproductComponent } from './updateproduct/updateproduct.component'
 import { ViewrequestComponent } from './viewrequest/viewrequest.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
-// routing navigation rules 
-// http://localhost:4200
-//admin routes 
-const routes: Routes = [
+const routes: Routes= [
+  { path: '', component: UserSignInComponent},
+  { path: 'signup', component: UserSignUpComponent},
   { path: "adminlogin", component: AdminloginComponent },
-
+  //{ path: "", redirectTo: "adminlogin", pathMatch: "prefix" },
+  { path: "**", component: NotFoundComponent },
   {
     path: "adminhome/:uname", component: AdminhomeComponent,
 
@@ -35,33 +28,31 @@ const routes: Routes = [
 
 
   },
-
-
-
-
-
-  // { path: "adminhome/:uname", component: AdminhomeComponent }, // uname is use to receive the value from a path 
-
-  { path: "", redirectTo: "adminlogin", pathMatch: "prefix" },
-  { path: "**", component: NotFoundComponent }
-
-
-  //end of admin routes 
-
-
-
-
 ];
 
 
-
-
-
-
-
->>>>>>> fcfd8fd173c56c2b7f64a552eae3229702ad9f82
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+
+// routing navigation rules 
+// http://localhost:4200
+//admin routes 
+// const routes: Routes = [
+
+
+
+//   // { path: "adminhome/:uname", component: AdminhomeComponent }, // uname is use to receive the value from a path 
+
+//   { path: "", redirectTo: "adminlogin", pathMatch: "prefix" },
+//   { path: "**", component: NotFoundComponent }
+
+
+//   //end of admin routes 
+
+// ];
+
+
--- grocers/src/app/app.module.ts
+++ grocers/src/app/app.module.ts
@@ -3,15 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { UserSignInComponent } from './user-sign-in/user-sign-in.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 import { UserSignUpComponent } from './user-sign-up/user-sign-up.component';
 import { AddEmployeeComponent } from './add-employee/add-employee.component';
 import { DeleteEmployeeComponent } from './delete-employee/delete-employee.component';
 import { GenerateReportsComponent } from './generate-reports/generate-reports.component';
-=======
 import { AdminloginComponent } from './adminlogin/adminlogin.component';
 import { AdminhomeComponent } from './adminhome/adminhome.component';
 import { AddproductComponent } from './addproduct/addproduct.component';
@@ -22,34 +19,22 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-<<<<<<< HEAD
->>>>>>> fcfd8fd173c56c2b7f64a552eae3229702ad9f82
-=======
-import { UnlockUsersComponent } from './employee/unlock-users/unlock-users.component';
-import { EditEmployeeProfileComponent } from './employee/edit-employee-profile/edit-employee-profile.component';
-import { PendingOrdersComponent } from './employee/update-order-status/pending-orders/pending-orders.component';
-import { ShippedOrdersComponent } from './employee/update-order-status/shipped-orders/shipped-orders.component';
-import { DeliveredOrdersComponent } from './employee/update-order-status/delivered-orders/delivered-orders.component';
-import { CancelledOrdersComponent } from './employee/update-order-status/cancelled-orders/cancelled-orders.component';
-import { OutForDeliveryOrdersComponent } from './employee/update-order-status/out-for-delivery-orders/out-for-delivery-orders.component';
->>>>>>> 30ba3366f0389f1efc52734e994fde69c4631721
+import { UnlockUsersComponent } from './unlock-users/unlock-users.component';
+import { EditEmployeeProfileComponent } from './edit-employee-profile/edit-employee-profile.component';
+import { PendingOrdersComponent } from './update-order-status/pending-orders/pending-orders.component';
+import { ShippedOrdersComponent } from './update-order-status/shipped-orders/shipped-orders.component';
+import { DeliveredOrdersComponent } from './update-order-status/delivered-orders/delivered-orders.component';
+import { CancelledOrdersComponent } from './update-order-status/cancelled-orders/cancelled-orders.component';
+import { OutForDeliveryOrdersComponent } from './update-order-status/out-for-delivery-orders/out-for-delivery-orders.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
     UserSignInComponent,
     UserSignUpComponent,
     AddEmployeeComponent,
     DeleteEmployeeComponent,
     GenerateReportsComponent,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    ReactiveFormsModule,
-=======
     AddproductComponent,
     AdminhomeComponent,
     AdminloginComponent,
@@ -57,17 +42,21 @@ import { OutForDeliveryOrdersComponent } from './employee/update-order-status/ou
     ViewrequestComponent,
     NotFoundComponent,
     UpdateproductComponent,
+
+  ],
+  imports: [
+    BrowserModule, 
+    AppRoutingModule, 
+    ReactiveFormsModule, 
+    HttpClientModule, 
+    BrowserAnimationsModule,
     UnlockUsersComponent,
     EditEmployeeProfileComponent,
     PendingOrdersComponent,
     ShippedOrdersComponent,
     DeliveredOrdersComponent,
     CancelledOrdersComponent,
-    OutForDeliveryOrdersComponent,
-  ],
-  imports: [
-    BrowserModule, AppRoutingModule, ReactiveFormsModule, HttpClientModule, BrowserAnimationsModule,
->>>>>>> fcfd8fd173c56c2b7f64a552eae3229702ad9f82
+    OutForDeliveryOrdersComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
